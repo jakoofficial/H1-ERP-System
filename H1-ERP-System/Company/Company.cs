ac199897748219ba440634fe64a09bf707362d2c
LavenderPixl <annajacobsen1999@hotmail.com>
2023-03-22 14:46:46 +0100

    D1 - Constructor added.

--- H1-ERP-System/Company/Company.cs
+++ H1-ERP-System/Company/Company.cs
@@ -6,15 +6,26 @@ using System.Threading.Tasks;
 
 namespace H1_ERP_System.Virksomhed
 {
-    public enum Currency { DKK, USD, EURO}
+    public enum Currency { DKK, USD, EUR}
     public class Company
     {
         public string CompanyName { get; set; } 
         public string Street { get; set; }
         public string StreetNumber { get; set; }
-        public string ZipCode { get; set; }
+        public string PostalCode { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public Currency Currency { get; set; }
+
+        public Company(string companyName, string street, string streetNumber, string postalCode, string city, string country, Currency currency)
+        {
+            CompanyName = companyName;
+            Street = street;
+            StreetNumber = streetNumber;
+            PostalCode = postalCode;
+            City = city;
+            Country = country;
+            Currency = currency;
+        }
     }
 }
