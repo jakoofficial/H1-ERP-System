d60a57f968dcf542169e45c744ba5dca294a5286
Jaco296c <jaco296c@outlook.dk>
2023-03-22 14:14:28 +0100

    Almost finished D6 & D7. Need to Merge with Product Branch to finish the Task(D6 D7).
    Need the list of Products to finish SaleOrderPrice and SaleOrderLine.

---
+++ H1-ERP-System/Sales/SalesOrderHeader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP_System.SalgsModul
+{
+    public enum Stage { None, Created, Confirmed, Packaged, Completed }
+
+
+    public class SalesOrderHeader
+    {
+        public int OrderNumber { get; set; } // Ordernummer
+        public string TimeCreated { get; set; } // Oprettelsetidspunkt
+        public string ImplementationTime { get; set; } //Gennemførelsetidspunkt
+
+        public int CustomerId { get; set; } //Kundenummer
+
+        public Stage Stage { get; set; } //Tilstand
+
+        List<SaleOrderLine> OrderLines = new List<SaleOrderLine>();  //En liste af orderlinjer
+        
+
+        public SalesOrderHeader(int ordernumber, string timecreated, string implementationtime, int customerId, Stage stage)
+        {
+            OrderNumber = ordernumber;
+            TimeCreated = timecreated;
+            ImplementationTime = implementationtime;
+            CustomerId = customerId;
+            Stage = stage;
+        }
+
+        public int SaleOrderPrice(int Price)
+        {
+           foreach (SaleOrderLine Order in OrderLines)
+            {
+                
+            }
+            return Price;
+        }
+        
+    }
+}
