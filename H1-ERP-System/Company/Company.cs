f09bb6a9324a90acf198fa97adb3e46d3ae4952e
LavenderPixl <annajacobsen1999@hotmail.com>
2023-03-22 14:21:49 +0100

    D1 - Created the class "Company". The class has properties, containing a Company's information. including an enum called Currency, which shows which currency we're working with (Dkk, USD, Eur).

---
+++ H1-ERP-System/Company/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_ERP_System.Virksomhed
+{
+    public enum Currency { DKK, USD, EURO}
+    public class Company
+    {
+        public string CompanyName { get; set; } 
+        public string Street { get; set; }
+        public string StreetNumber { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public Currency Currency { get; set; }
+    }
+}
