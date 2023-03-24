ad3d097654e42fb1f63878c6c3066eac0552d410
LavenderPixl <annajacobsen1999@hotmail.com>
2023-03-24 13:57:16 +0100

    V1 - Started working on CompanyScreen.
    Todo is a temporary testing class, will be deleted.
    
    Committing and pushing to Company branch to keep the work, + merging with Master, to get the newest version/newest methods, to use with the upcoming CompanyScreen.

---
+++ H1-ERP-System/UI/CompanyScreen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TECHCOOL.UI;
+using static H1_ERP_System.UI.Todo;
+
+namespace H1_ERP_System.UI
+{
+    public class CompanyScreen : Screen
+    {
+        public override string Title { get; set; } = "CompanyScreen";
+        protected override void Draw()
+        {
+            Clear(this);
+            ListPage<Todo> listPage = new ListPage<Todo>();
+
+            listPage.Add(new Todo("nr1", 1, Todo.TodoState.Todo));
+            listPage.Add(new Todo("nr2", 2, TodoState.Todo));
+            listPage.Add(new Todo("nr3", 3, TodoState.Todo));
+            listPage.AddColumn("Company name", "Title");
+            listPage.AddColumn("Country", "Priority");
+            listPage.AddColumn("Currency", "State");
+
+            listPage.Draw();
+        }
+    }
+}
