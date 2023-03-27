2a3335383509fe5db7fc4b1ff7adf97afa4a1dab
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-03-27 11:19:51 +0200

    Moved Methods from Base Database class to the various folder database classes

--- H1-ERP-System/CompanyFolder/Database.cs
+++ H1-ERP-System/CompanyFolder/Database.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using H1_ERP_System.CompanyFolder;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,34 @@ namespace H1_ERP_System
 {
     public partial class Database
     {
-        
+        /// <summary>
+        /// Used to get a list of companies from the database table 
+        /// </summary>
+        /// <param name="queryString"></param>
+        /// <returns></returns>
+        public static List<Company> GetCompanies(string queryString)
+        {
+            if (!queryString.IsNullOrEmpty())
+            {
+                List<Company> cList = new List<Company>();
+                using (SqlConnection connection = Instance.GetConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    //Debug.WriteLine("Connectoin {0}", connection.State);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Company cp = new Company((int)reader[0], (string)reader[1], GetAddress($"SELECT * FROM dbo.Address WHERE AddressId = {(int)reader[2]}"), (Company.Currencies)(int)reader[3]);
+                            cList.Add(cp);
+                        }
+                    }
+                }
+                return cList;
+            }
+            return null;
+        }
     }
 }
--- H1-ERP-System/CustomerFolder/Database.cs
+++ H1-ERP-System/CustomerFolder/Database.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using H1_ERP_System.CustomerFolder;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,27 @@ namespace H1_ERP_System
 {
     public partial class Database
     {
+        public static Address GetAddress(string queryString)
+        {
+            if (!queryString.IsNullOrEmpty())
+            {
+                using (SqlConnection connection = Instance.GetConnection())
+                {
+                    SqlCommand cmd = new SqlCommand(queryString, connection);
+                    connection.Open();
+                    //Debug.WriteLine("Connectoin {0}", connection.State);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Address adr = new Address((int)reader[0], (string)reader[1], (string)reader[2], (string)reader[3], (string)reader[4], (string)reader[5]);
+                            return adr;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
