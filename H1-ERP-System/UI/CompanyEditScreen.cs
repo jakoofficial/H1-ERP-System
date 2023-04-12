00ba58b6b9b136d5499a9d4e78384e1a8e871e49
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-04-12 09:22:57 +0200

    V1, V2, V3 ---------------- Editing of the company saves and updates the company + Address on the DB

--- H1-ERP-System/UI/CompanyEditScreen.cs
+++ H1-ERP-System/UI/CompanyEditScreen.cs
@@ -1,8 +1,10 @@
 ﻿using H1_ERP_System.CompanyFolder;
+using H1_ERP_System.CustomerFolder;
 using MySqlX.XDevAPI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -27,21 +29,42 @@ namespace H1_ERP_System.UI
             EditCompany(CP);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="cp"></param>
         public static void EditCompany(Company cp)
         {
             //var props = new Dictionary<string, object>();
             Form<Company> cpEditor = new Form<Company>();
 
             cpEditor.TextBox($"Company Name ", "CompanyName");
+            cpEditor.TextBox("Country", "Country");
+            cpEditor.TextBox("City", "City");
+            cpEditor.TextBox("PostalCode", "PostalCode");
+            cpEditor.TextBox("StreetNumber", "StreetNumber");
+            cpEditor.TextBox("Street", "Street");
             cpEditor.SelectBox($"Currency", "Currency");
             cpEditor.AddOption($"Currency", "EUR", Company.Currencies.EUR);
             cpEditor.AddOption($"Currency", "USD", Company.Currencies.USD);
-            cpEditor.AddOption($"Currency", "Dkk", Company.Currencies.DKK);
+            cpEditor.AddOption($"Currency", "DKK", Company.Currencies.DKK);
 
             Console.WriteLine("Press ESC When Done\n");
             cpEditor.Edit(cp);
+
+            Database.UpdateCompany(cp);
+            
+            Address adr = Database.GetAddress($"SELECT * FROM dbo.Address WHERE AddressId = {cp.Address.Id}");
+            adr.StreetNumber = cp.StreetNumber; 
+            adr.Street = cp.Street;
+            adr.City = cp.City; 
+            adr.PostalCode = cp.PostalCode;
+            adr.Country = cp.Country;
+
+            Database.UpdateAddress(adr);
+
             Clear();
-            Console.WriteLine("\n Done");
+            Console.WriteLine("\n Successfully Updated");
             Console.ReadLine();
             Screen.Display(new CompanyScreen());
         }
