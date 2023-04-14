3add73df3eecc5e2cebb6cb7e319cc586352c70f
Matkip0 <biegel987@gmail.com>
2023-04-14 08:51:26 +0200

    K2 and K3 done

--- H1-ERP-System/CustomerFolder/Customer.cs
+++ H1-ERP-System/CustomerFolder/Customer.cs
@@ -13,16 +13,32 @@ namespace H1_ERP_System.CustomerFolder
         public string LastPurchase { get; set; } //Sidste købte dato
         public string FullName { get; private set; }
 
+        ///// <summary>
+        ///// For when a customer needs to be created
+        ///// </summary>
+        ///// <param name="customerId">Int : The ID of the customer</param>
+        ///// <param name="lastPurchase">DateTime : The date of last purchase</param>
+        ///// <param name="firstName">String : First name of the customer</param>
+        ///// <param name="lastName">String : Last name of the customer</param>
+        ///// <param name="address">String : The address of the customer</param>
+        ///// <param name="phoneNumber">String : The phone number of the customer</param>
+        ///// <param name="email">String : The customer's E-mail</param>
+
         /// <summary>
         /// For when a customer needs to be created
         /// </summary>
-        /// <param name="customerId">Int : The ID of the customer</param>
-        /// <param name="lastPurchase">DateTime : The date of last purchase</param>
-        /// <param name="firstName">String : First name of the customer</param>
-        /// <param name="lastName">String : Last name of the customer</param>
-        /// <param name="address">String : The address of the customer</param>
-        /// <param name="phoneNumber">String : The phone number of the customer</param>
-        /// <param name="email">String : The customer's E-mail</param>
+        /// <param name="customerId">The ID of the customer</param>
+        /// <param name="lastPurchase">The date of last purchase</param>
+        /// <param name="firstName">First name of the customer</param>
+        /// <param name="lastName">Last name of the customer</param>
+        /// <param name="phoneNumber">The phone number of the customer</param>
+        /// <param name="email">The customer's E-mail</param>
+        /// <param name="addressId"></param>
+        /// <param name="street">The street address</param>
+        /// <param name="streetNumber">the street number</param>
+        /// <param name="postalCode">Postal code for the address</param>
+        /// <param name="city">City where address is located</param>
+        /// <param name="country">Country where address is located</param>
         public Customer(int customerId, string lastPurchase, string firstName, string lastName, string phoneNumber, string email, int addressId, string street, string streetNumber, string postalCode, string city, string country) : base(firstName, lastName, phoneNumber, email, addressId, street, streetNumber, postalCode, city, country)
         {
             CustomerId = customerId;
