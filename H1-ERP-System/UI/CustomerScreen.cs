3230e65c77764d41383f7e6f2b5c87e2c219aaca
Jaco296c <jaco296c@outlook.dk>
2023-04-19 13:46:25 +0200

    Fixed Names to fit new Database. - Created a if to check if a Customer already exist or else it takes you to the method to create one.

--- H1-ERP-System/UI/CustomerScreen.cs
+++ H1-ERP-System/UI/CustomerScreen.cs
@@ -11,42 +11,47 @@ namespace H1_ERP_System.UI
 {
     public class CustomerScreen : Screen
     {
-        public override string Title { get; set; } = "Customerlist";
+        public override string Title { get; set; } = "Customer List";
 
         #region CustomerScreen
         protected override void Draw()
         {
-            Console.Clear();
-            Console.WriteLine("Enter  | Select\n" +
-                              "F2     | Create new\n" +
-                              "ESC    | Go back");
 
             ListPage<Customer> customerListPage = new ListPage<Customer>();
             List<Customer> list = Database.GetCustomerList();
-            foreach (Customer customer in list)
-                customerListPage.Add(customer);
-
-            customerListPage.AddColumn("Customers ID", "CustomerId");
-            customerListPage.AddColumn("Name", "FullName");
-            customerListPage.AddColumn("PhoneNumber", "PhoneNumber");
-            customerListPage.AddColumn("Email", "Email");
-            customerListPage.AddKey(ConsoleKey.F2, CustomerEditScreen.CreateCustomer);
-            
-            
-            Console.WriteLine("Enter  | Select\n" +
-                              "F2     | Create new\n" +
-                              "ESC    | Go back");
-
-            Customer selected = customerListPage.Select();
-
-            if (selected != null)
+            if (list.Count != 0 ) 
             {
-                Title = $"{selected.FirstName} {selected.LastName}";
-                Clear(this);
-                CustomerDetails(selected);
+                foreach (Customer customer in list)
+                    customerListPage.Add(customer);
+
+                customerListPage.AddColumn("Customers ID", "CustomerId");
+                customerListPage.AddColumn("Name", "FullName");
+                customerListPage.AddColumn("PhoneNumber", "PhoneNumber");
+                customerListPage.AddColumn("Email", "Email");
+                customerListPage.AddKey(ConsoleKey.F2, CustomerEditScreen.CreateCustomer);
+
+
+                Console.WriteLine("Enter  | Select\n" +
+                                  "F2     | Create new\n" +
+                                  "ESC    | Go back");
+
+                Customer selected = customerListPage.Select();
+
+                if (selected != null)
+                {
+                    Title = $"{selected.FirstName} {selected.LastName}";
+                    Clear(this);
+                    CustomerDetails(selected);
+                }
+                else
+                    Quit();
             }
             else
-                Quit();
+            {
+                CustomerEditScreen.CreateCustomer(new Customer());
+            }
+
+            
         }
         #endregion
 
